b653d8e2cbf2256884edf652e22b470dfe929d36
catalinpuricoi15 <61915381+catalinpuricoi15@users.noreply.github.com>
2021-04-09 01:24:11 +0300

    refactor(MOD-4): complete the documents page

--- hacks-web/resources/views/documents/index.blade.php
+++ hacks-web/resources/views/documents/index.blade.php
@@ -9,27 +9,27 @@
         </div>
     </section>
 
-    <section class="row docs-section">
-        <div class="text-center">
-            <h3 class="docs-section-title">Carte de identitate</h3>
+
+    <section class="docs-section ">
+        <div>
+            <h3 class="col-sm-5 docs-section-title text-center">Carte de identitate</h3>
         </div>
-        <div class="col-12">
-            <div class="row">
-                <div class="col-8">
+        <div class="row">
+            <div class="col-12 col-md-8">
+                <div class="docs-info-container">
                     <div class="dropdown show">
                         <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Documente necesare
 
                         </a>
-
                         <ol class="dropdown-menu docs-dropdown col-12" aria-labelledby="dropdownMenuLink">
 
-                            <li class="dropdown-item" >- Cerere pentru eliberarea actului de identitate</li>
-                            <li class="dropdown-item" >- Certificatul de nastere(fotocopie si original)</li>
+                            <li class="dropdown-item">- Cerere pentru eliberarea actului de identitate</li>
+                            <li class="dropdown-item">- Certificatul de nastere(fotocopie si original)</li>
 
-                            <li class="dropdown-item" >- Documentul cu care solicitantul face dovada adresei de
+                            <li class="dropdown-item">- Documentul cu care solicitantul face dovada adresei de
                                 domiciliu</li>
-                            <li class="dropdown-item" >- Daca gazduitorul nu se poate prezenta la serviciul
+                            <li class="dropdown-item">- Daca gazduitorul nu se poate prezenta la serviciul
                                 public comunitar de evidenta a persoanelor, declaratia de primire in spatiu poate fi
                                 consemnata la notarul public, la misiunea diplomatica sau oficiul consular al Romaniei
                                 din strainatare sauu in prezenta politistului de la postul de politie pentru mediul
@@ -37,38 +37,72 @@
 
                         </ol>
                     </div>
-                </div>
-                <div class="col-4">
-                    <img src="/images/svg/document.svg">
+                    <div class="dropdown show">
+                        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Conditii generale
+
+                        </a>
+
+                        <ol class="dropdown-menu docs-dropdown col-12" aria-labelledby="dropdownMenuLink">
+
+                            <li class="dropdown-item">- Cerere pentru eliberarea actului de identitate</li>
+                            <li class="dropdown-item">- Certificatul de nastere(fotocopie si original)</li>
+
+                            <li class="dropdown-item">- Documentul cu care solicitantul face dovada adresei de
+                                domiciliu</li>
+                            <li class="dropdown-item">- Daca gazduitorul nu se poate prezenta la serviciul
+                                public comunitar de evidenta a persoanelor, declaratia de primire in spatiu poate fi
+                                consemnata la notarul public, la misiunea diplomatica sau oficiul consular al Romaniei
+                                din strainatate sau in prezenta politistului de la postul de politie pentru mediul
+                                rural.</li>
+
+                        </ol>
+                    </div>
+                    <div class="dropdown show">
+                        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Instiutiile care
+                            elibereaza
+
+                        </a>
+
+                        <ol class="dropdown-menu docs-dropdown col-12" aria-labelledby="dropdownMenuLink">
+
+                            <li class="dropdown-item">- Serivciul public comunitar de evidenta a persoanelor.</li>
+
+                        </ol>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div class="col-md-4 sm-hidden ">
+                <img class="docs-img" src="/images/svg/document.svg">
+            </div>
+
     </section>
 </div>
-<section class="container-fluid bg-primary-accent mb-5">
-    <div class="row p-5 align-items-center justify-content-center ">
-        <div class="col-12 d-flex justify-content-center">
-            <h3 class="text-white">Feedback-ul tau conteaza</h3>
-            <a href="/contact" class="btn ml-3 button-accent-secondary">Ofera feedback</a>
+<div class="container-fluid container-fluid-background my-5">
+    <div class="p-5">
+        <div class="align-feedback">
+            <h2>Feedback-ul tau conteaza !</h2>
+            <a href="#" class="btn button-accent-secondary">Ofera feedback</a>
         </div>
     </div>
-</section>
-<section class="container">
-    <div class="row">
-        <div class="text-center">
-            <h3 class="docs-section-title">Pro Tips</h3>
+
+
+</div>
+<div class="container">
+        <div>
+            <h3 class="col-sm-3 docs-section-title text-center">Pro Tips</h3>
         </div>
-    </div>
-    <div class="row mt-5">
         <div class="col-12">
             <ol class="panel ">
 
-                <li class="panel-item"><img src="/images/svg/alert.svg"> Cerere pentru eliberarea actului de identitate</li>
-
+            
+                <li class="panel-item"><img style="margin-right: 10px" src="/images/svg/alert.svg"> Cerere pentru eliberarea actului de identitate
+                <li class="panel-item"><img style="margin-right: 10px" src="/images/svg/alert.svg"> Cerere pentru eliberarea actului de identitate
+                <li class="panel-item"><img style="margin-right: 10px" src="/images/svg/alert.svg"> Cerere pentru eliberarea actului de identitate
 
             </ol>
         </div>
-    </div>
-</section>
+</div>
 
 @endsection
