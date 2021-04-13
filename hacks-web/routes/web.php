54c410e9c77de22fb032b2bf364b540b1d617c7f
catalinpuricoi15 <61915381+catalinpuricoi15@users.noreply.github.com>
2021-04-13 18:16:26 +0300

    Feat(MOD-9): Add dashboard view

--- hacks-web/routes/web.php
+++ hacks-web/routes/web.php
@@ -38,3 +38,5 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/documents', [App\Http\Controllers\DocsController::class, 'index']);
+
+Route::get('/admin/dashboard', [App\Http\Controllers\AdminController::class, 'index']);
