a203468fd70efd6800603d289836209f44d15388
catalinpuricoi15 <61915381+catalinpuricoi15@users.noreply.github.com>
2021-04-14 21:09:00 +0300

    Refactor(MOD-9):addTopNavbar

--- hacks-web/resources/views/admin/dashboard.blade.php
+++ hacks-web/resources/views/admin/dashboard.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.auth')
 
 @section('content')
+<div class="main-box col-12">
 
+
+    <div id="class-123" class="class" onclick='goToClass("123")'>
+        <div class="class-name"> </div>
+        <div class="class-info">
+            <div class="class-id"> #123 </div>
+            <div class="class-supervisor"> Prof. Cosmin Varlan <br> Prof. Buraga Sabin </div>
+        </div>
+    </div>
+
+
+    <div id="class-321" class="class" onclick='goToClass("321")'>
+        <div class="class-name"> SGBD </div>
+        <div class="class-info">
+            <div class="class-id"> #321 </div>
+            <div class="class-supervisor"> Prof. Cosmin Varlan</div>
+        </div>
+    </div>
+
+</div>
 @endsection
--- hacks-web/resources/views/layouts/auth.blade.php
+++ hacks-web/resources/views/layouts/auth.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -22,13 +23,41 @@
     <!--  -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet" />
 </head>
+
 <body>
 
-<div id="app">
-    <main class="container-fluid p-0">
-        @include('admin.components.navbar')
-        @yield('content')
-    </main>
-</div>
+    <div id="app">
+        <main class="container-fluid p-0">
+            <div class="row">
+                @include('admin.components.navbar')
+                <div class="col-11">
+                    <div class="row">
+                        <div id="header" class="header col-12">
+                            <div class="logo">
+                                <img src='/images/home/logo.svg' class="nav-image-size">
+                            </div>
+                            <div class="dropdown dropleft">
+                                <div class="d-flex align-items-center">
+                                    <span class="mr-3">Admin</span>
+                                    <img src='/images/svg/user.svg' class="imageAdmin dropdown-toggle "
+                                        id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                                        aria-expanded="false">
+                                </div>
+                                <div class="dropdown-menu mr-2 dropdown-menu-left" aria-labelledby="dropdownMenuButton"
+                                    aria-labelledby="dropdownMenuOffset">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        @yield('content')
+                    </div>
+                </div>
+        </main>
+    </div>
 </body>
+
 </html>
