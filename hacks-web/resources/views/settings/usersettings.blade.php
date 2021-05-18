263cdba92b04e3163df656999b36e1ad18d48996
SanduVAdelin <adelinsandu26@gmail.com>
2021-05-18 15:26:52 +0300

    traducere site

--- hacks-web/resources/views/settings/usersettings.blade.php
+++ hacks-web/resources/views/settings/usersettings.blade.php
@@ -5,62 +5,61 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-12 col-lg-10 col-xl-8 mx-auto">
-                <h2 class="h3 mb-4 page-title ml-1">User settings</h2>
+                <h2 class="h3 mb-4 page-title ml-1">Setările contului</h2>
                 <div class="my-4">
                     <ul class="nav nav-tabs mb-4" id="myTab" role="tablist">
                         <li class="nav-item">
                             <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab"
-                               aria-controls="home" aria-selected="false">My Profile</a>
+                               aria-controls="home" aria-selected="false">Profilul meu</a>
                         </li>
                     </ul>
                     <form>
                         <div class="form-row">
                             <div class="form-group col-md-6">
-                                <label for="firstname">Firstname</label>
-                                <input type="text" id="firstname" class="form-control" placeholder="Firstname"/>
+                                <label for="firstname">Prenume</label>
+                                <input type="text" id="firstname" class="form-control" placeholder="Prenume"/>
                             </div>
                             <div class="form-group col-md-6">
-                                <label for="lastname">Lastname</label>
-                                <input type="text" id="lastname" class="form-control" placeholder="Lastname"/>
+                                <label for="lastname">Nume</label>
+                                <input type="text" id="lastname" class="form-control" placeholder="Nume"/>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="inputEmail4">Email</label>
                             <input type="email" class="form-control" id="inputEmail4" placeholder="email@gmail.com"/>
                         </div>
-                        <button type="submit" class="btn btn-primary">Save Change</button>
+                        <button type="submit" class="btn btn-primary">Salvează schimbările</button>
                     </form>
 
                     <form class="mt-5">
                         <ul class="nav nav-tabs mb-4" id="myTab" role="tablist">
                             <li class="nav-item">
                                 <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab"
-                                   aria-controls="home" aria-selected="false">Change Password</a>
+                                   aria-controls="home" aria-selected="false">Schimbă parola</a>
                             </li>
                         </ul>
                         <div class="row mb-4">
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <label for="inputPassword5">New Password</label>
+                                    <label for="inputPassword5">Parolă nouă</label>
                                     <input type="password" class="form-control" id="inputPassword5"/>
                                 </div>
                                 <div class="form-group">
-                                    <label for="inputPassword6">Confirm Password</label>
+                                    <label for="inputPassword6">Confirmă parola</label>
                                     <input type="password" class="form-control" id="inputPassword6"/>
                                 </div>
                             </div>
                             <div class="col-md-6">
-                                <p class="mb-2">Password requirements</p>
-                                <p class="small text-muted mb-2">To create a new password, you have to meet all of the
-                                    following requirements:</p>
+                                <p class="mb-2">Cerințele parolei</p>
+                                <p class="small text-muted mb-2">Pentru a crea o nouă parolă, trebuie să respecți următoarele cerințe : </p>
                                 <ul class="small text-muted pl-4 mb-0">
-                                    <li>Minimum 8 character</li>
-                                    <li>At least one special character</li>
-                                    <li>At least one number</li>
+                                    <li>Minim 8 caractere</li>
+                                    <li>Cel puțin un caracter special</li>
+                                    <li>Cel puțin un număr</li>
                                 </ul>
                             </div>
                         </div>
-                        <button type="submit" class="btn btn-primary">Change password</button>
+                        <button type="submit" class="btn btn-primary">Schimbă parola</button>
                     </form>
                 </div>
             </div>
