2319fcdf6fafe901ac15680adc3683bdbd713a02
catalinpuricoi15 <61915381+catalinpuricoi15@users.noreply.github.com>
2021-05-20 23:11:51 +0300

    Change(MOD-49):ReviewsAdmin

--- hacks-web/resources/js/components/ReviewsContainer.vue
+++ hacks-web/resources/js/components/ReviewsContainer.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="row feedback-container-admin">
-    <a class="feedback-admin" v-for="(review,index) in reviews" :key="index" @click="showReview(index)">
-      <div class="feedback-name-admin"><span>{{ review.username }}</span></div>
+  <div class="row feedback-container-admin" v-if="isGlobalAdmin">
+    <a
+      class="feedback-admin"
+      v-for="(review, index) in reviews"
+      :key="index"
+      @click="showReview(index)"
+    >
+      <div class="feedback-name-admin">
+        <span>{{ review.username }}</span>
+      </div>
       <div class="feedback-info-admin">
         <p class="feedback-text" id="feedback-text">
-          {{ review.comment.substring(1,150) }}...
+          {{ review.comment.substring(0, 150) }}...
         </p>
         <p class="feedback-date-admin mr-1" id="feedback-date-admin">
           Rating: {{ review.rating }}
@@ -12,47 +19,89 @@
       </div>
     </a>
   </div>
+  <div class="row feedback-container-admin" v-else>
+    <a
+      class="feedback-admin"
+      v-for="(review, index) in reviews.feedbacks"
+      :key="index"
+      @click="showReview(index)"
+    >
+      <div class="feedback-name-admin">
+        <span>{{ review.username }}</span>
+      </div>
+      <div class="feedback-info-admin">
+        <p class="feedback-text" id="feedback-text">
+          {{ review.comment.substring(0, 150) }}...
+        </p>
+      </div>
+    </a>
+  </div>
 </template>
 <script>
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 export default {
   name: "ReviewsContainer",
-  components: {Swal},
+  components: { Swal },
+  props: ["userType"],
   data() {
     return {
       reviews: [],
     };
   },
   created() {
-    axios.get("/get-reviews").then((response) => {
-      this.reviews = response.data.reviews;
-
-    });
-  },
-  methods:{
-      showReview(index){
-          Swal.fire({
-              title:this.reviews[index].username,
-              html:`<p>${this.reviews[index].comment}</p>`,
-              showDenyButton: true,
-              confirmButtonText:"gata",
-              denyButtonText: "delete"
-          }).then((result) => {
-            if(!(result.isConfirmed)){
-              axios.post("/delete-reviews", {index: index}).then((response)=>{
-                console.log(response);
-              })
-            }
-          })
-      },
-  },
-
-    computed: {
-        reviewsArray() {
-            return reviews.filter((review)=>{
-                return review;
-            })
-        }
+    if (this.isGlobalAdmin) {
+      axios.get("/get-reviews").then((response) => {
+        this.reviews = response.data.reviews;
+      });
+    } else {
+      axios
+        .get(
+          `/get-reviews-by-institution?i=${this.userType.institutions[0].name}`
+        )
+        .then((response) => {
+          this.reviews = response.data;
+        });
     }
+  },
+  methods: {
+    showReview(index) {
+      if (this.isGlobalAdmin) {
+        Swal.fire({
+          title: this.reviews[index].username,
+          html: `<p>${this.reviews[index].comment}</p>`,
+          showDenyButton: true,
+          confirmButtonText: "gata",
+          denyButtonText: "delete",
+        }).then((result) => {
+          if (!result.isConfirmed) {
+            axios.post("/delete-reviews", { index: index }).then((response) => {
+              console.log(response);
+            });
+          }
+        });
+      } else {
+        Swal.fire({
+          title: this.reviews.feedbacks[index].username,
+          html: `<strong>${this.reviews.feedbacks[index].process}</strong> <p class="mt-4">${this.reviews.feedbacks[index].comment}</p> `,
+          confirmButtonText: "gata",
+        });
+      }
+    }
+  },
+
+  computed: {
+    isGlobalAdmin() {
+      console.log(this.userType.institutions.length);
+      if (this.userType.institutions.length != 0) {
+        return false;
+      }
+      return true;
+    },
+    reviewsArray() {
+      return reviews.filter((review) => {
+        return review;
+      });
+    },
+  },
 };
 </script>
