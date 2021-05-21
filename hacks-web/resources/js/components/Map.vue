f6249a5098d5481a4a5195154f31b6e07015c8d6
Claudiu Popa <claudiupopa330@gmail.com>
2021-05-21 08:44:37 +0300

    fix(dev): fix map issue

--- hacks-web/resources/js/components/Map.vue
+++ hacks-web/resources/js/components/Map.vue
@@ -6,10 +6,11 @@
 
 <script>
 import Swal from 'sweetalert2';
+
 export default {
     name: "Map",
-    props:['generateRoute','requestData'],
-    components:{Swal},
+    props: ['generateRoute', 'requestData'],
+    components: {Swal},
     data() {
         return {
             map: [],
@@ -18,7 +19,7 @@ export default {
             initialMarker: [],
             currentLatitude: 47.17,
             currentLongitude: 27.57,
-            popupOffsets : {
+            popupOffsets: {
                 top: [0, 0],
                 bottom: [0, -70],
                 'bottom-right': [0, -70],
@@ -28,17 +29,17 @@ export default {
             }
         };
     },
-    watch:{
-      generateRoute: function(){
-          this.currentLatitude = this.requestData.latitude;
-          this.currentLongitude =this.requestData.longitude;
-          this.map.setCenter([this.currentLongitude, this.currentLatitude]);
-          this.initialMarker.remove();
-          this.initialMarker  = new tt.Marker().setLngLat([this.currentLongitude,this.currentLatitude]).addTo(this.map);
-          var popup = new tt.Popup({offset: this.popupOffsets}).setHTML("<b>Poziția ta actuală</b> ");
-          this.initialMarker .setPopup(popup).togglePopup();
-          this.getRoute();
-      }
+    watch: {
+        generateRoute: function () {
+            this.currentLatitude = this.requestData.latitude;
+            this.currentLongitude = this.requestData.longitude;
+            this.map.setCenter([this.currentLongitude, this.currentLatitude]);
+            this.initialMarker.remove();
+            this.initialMarker = new tt.Marker().setLngLat([this.currentLongitude, this.currentLatitude]).addTo(this.map);
+            var popup = new tt.Popup({offset: this.popupOffsets}).setHTML("<b>Poziția ta actuală</b> ");
+            this.initialMarker.setPopup(popup).togglePopup();
+            this.getRoute();
+        }
     },
     mounted() {
         this.map = tt.map({
@@ -49,37 +50,45 @@ export default {
             center: [this.currentLongitude, this.currentLatitude]
         });
 
-        this.initialMarker = new tt.Marker().setLngLat([this.currentLongitude,this.currentLatitude]).addTo(this.map);
+        this.initialMarker = new tt.Marker().setLngLat([this.currentLongitude, this.currentLatitude]).addTo(this.map);
         var popup = new tt.Popup({offset: this.popupOffsets}).setHTML("<b>Iasi</b> ");
         this.initialMarker.setPopup(popup).togglePopup();
 
     },
     methods: {
         getRoute() {
-            this.requestData.latitude =  this.currentLongitude;
+            this.requestData.latitude = this.currentLongitude;
             this.requestData.longitude = this.currentLatitude;
             console.log(this.requestData);
             axios.post('/get-route', this.requestData)
                 .then(response => {
                     this.data = response.data;
-                    var coords = this.data.features[this.data.features.length-3].geometry.coordinates;
+                    var coords = this.data.features[this.data.features.length - 3].geometry.coordinates;
 
 
-                    this.initialMarker = new tt.Marker().setLngLat([coords[0],coords[1]]).addTo(this.map);
-                    var popup = new tt.Popup({offset: this.popupOffsets}).setHTML(`<b>Destinatie</b> `);
-                    this.initialMarker.setPopup(popup).togglePopup();
 
-                    for(let i=1; i<this.data.features.length-3; i++){
-                        var coords2 = this.data.features[i].geometry.coordinates;
-                        let marker = new tt.Marker().setLngLat([coords2[0],coords2[1]]).addTo(this.map);
-                        var popup = new tt.Popup({offset: this.popupOffsets}).setHTML(`<b>Oprire</b> `);
-                        marker.setPopup(popup).togglePopup();
+
+                    for (let i = 1; i < this.data.features.length-1; i++) {
+
+                        if (this.data.features[i].geometry.type == "Point") {
+                            if( this.data.features[i+1].geometry.type != "LineString"){
+                                var coords2 = this.data.features[i].geometry.coordinates;
+                                let marker = new tt.Marker().setLngLat([coords2[0], coords2[1]]).addTo(this.map);
+                                var popup = new tt.Popup({offset: this.popupOffsets}).setHTML(`<b>Oprire</b> `);
+                                marker.setPopup(popup).togglePopup();
+                            }else{
+                                this.initialMarker = new tt.Marker().setLngLat([coords[0], coords[1]]).addTo(this.map);
+                                var popup = new tt.Popup({offset: this.popupOffsets}).setHTML(`<b>Destinatie</b> `);
+                                this.initialMarker.setPopup(popup).togglePopup();
+                            }
+                        }
                     }
 
                     this.displayRoute(this.data);
 
                 })
                 .catch((response) => {
+                    console.log(response);
                     Swal.fire({
                         title: "Oops...",
                         text: "A aparut o eroare",
